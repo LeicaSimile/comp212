06b403df35386c82f199dd5b9c2dbb2a948ca933
LeicaSimile <leicasimile@gmail.com>
2018-01-26 11:11:54 -0500

    Close window upon btnExit click

--- assignment01/assignment01/frmMain.cs
+++ assignment01/assignment01/frmMain.cs
@@ -16,5 +16,10 @@ namespace assignment01
         {
             InitializeComponent();
         }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }

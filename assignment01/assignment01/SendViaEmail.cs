5f015fe4dd119dd2ade5d2637a51ba65fc0d6a89
LeicaSimile <leicasimile@gmail.com>
2018-02-02 22:21:06 -0500

    Add delegate classes

---
+++ assignment01/assignment01/SendViaEmail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment01
+{
+    public class SendViaEmail
+    {
+        private String Email { get; set; }
+
+        public SendViaEmail() { }
+
+        public SendViaEmail(String email)
+        {
+            Email = email;
+        }
+
+        public void sendEmail(string msg)
+        {
+            MessageBox.Show(String.Format("Sent to {0}: '{1}'", Email, msg));
+        }
+
+        public void Subscribe(Publisher pub)
+        {
+            pub.publishmsg += sendEmail;
+        }
+    }
+}
---
+++ assignment01/assignment01/SendViaMobile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment01
+{
+    public class SendViaMobile
+    {
+        private String CellPhone { get; set; }
+
+        public SendViaMobile() { }
+
+        public SendViaMobile(String phone)
+        {
+            CellPhone = phone;
+        }
+
+        private void sendMessage(string msg)
+        {
+            MessageBox.Show(String.Format("Texted {0}: '{1}'", CellPhone, msg));
+        }
+
+        public void Subscribe(Publisher pub)
+        {
+            pub.publishmsg += sendMessage;
+        }
+    }
+}

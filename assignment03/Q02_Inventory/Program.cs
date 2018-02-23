e3f5e56ee1d35acd42965fe9279670c453964575
leicasimile <leicasimile@gmail.com>
2018-02-23 10:09:35 -0500

    Set up array of items

--- assignment03/Q02_Inventory/Program.cs
+++ assignment03/Q02_Inventory/Program.cs
@@ -10,6 +10,16 @@ namespace Q02_Inventory
     {
         static void Main(string[] args)
         {
+            InventoryItem[] inventory = {
+                    new InventoryItem(83, "Electric sander", 7, 59.98),
+                    new InventoryItem(24, "Power saw", 18, 99.99),
+                    new InventoryItem(7, "Sledge hammer", 11, 21.55),
+                    new InventoryItem(77, "Hammer", 76, 11.99),
+                    new InventoryItem(39, "Lawn mower", 3, 79.95),
+                    new InventoryItem(68, "Screwdriver", 106, 7.99),
+                    new InventoryItem(56, "Jig Saw", 21, 11.95),
+                    new InventoryItem(3, "Wrench", 34, 7.95),
+                };
         }
     }
 }

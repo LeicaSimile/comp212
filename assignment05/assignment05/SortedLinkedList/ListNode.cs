c6e5e6e2dd8a236dba139680db10bbd846a86276
Angelica Catalan <leicasimile@gmail.com>
2018-04-13 18:02:01 -0400

    Init question 2

---
+++ assignment05/assignment05/SortedLinkedList/ListNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinkedListLibrary
+{
+    public class ListNode<T>
+    {
+        public T Data { get; private set; }
+
+        public ListNode<T> Next { get; set; }
+
+        // constructor to create ListNode that refers to dataValue
+        // and is last node in list
+        public ListNode(T dataValue): this(dataValue, null){}
+      
+         // constructor to create ListNode that refers to dataValue
+        // and refers to next ListNode in List
+        public ListNode(T dataValue, ListNode<T> nextNode)
+        {
+            Data = dataValue;
+            Next = nextNode;
+        } // end constructor
+    } // end class ListNode
+}
